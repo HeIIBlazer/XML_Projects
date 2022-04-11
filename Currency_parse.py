43b878fd5b1ffab72ce4abf2ccc52c3dc0cddd02
Exorcist <90632640+NAGLISVIN2010@users.noreply.github.com>
2022-04-11 19:52:17 +0300

    done

--- Currency_parse.py
+++ Currency_parse.py
@@ -19,3 +19,31 @@ for item in root.findall('./country'):
 
     roots.append(currency)
 print(roots)
+
+
+print ( )
+ivkhk = ET.parse('Vassiljev_ivkhk.xml')  
+staff = ivkhk.getroot()
+
+staffs = []
+
+for item in staff.findall('./department'):
+    sphere = {}
+    sphere['Department'] = item.attrib['name']
+    for child in item:
+        if child.tag == 'speciality':
+            sphere['Speciality'] = child.attrib['name']
+            staffs.append(sphere)
+            for child1 in child:
+                if child1.tag == 'group':
+                    group={}
+                    group['Group'] = child1.attrib['name']
+                    staffs.append(group)
+                    for child2 in child1:
+                        if child2.tag == 'student':
+                            student = {}
+                            student['student'] = child2.text
+                            staffs.append(student)
+
+
+print(staffs)
